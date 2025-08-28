bb97200eb141dc121c35b1f9322d317ebc8735b5
Alejandro Alvarez <alvarez.h.alejo@gmail.com>
2025-08-27 21:35:08 -0400

    feat: agrego la funcionalidad generarCadenaFizzBuzz al index y al presenter para demostrar su ejecucion

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import { generarCadenaFizzBuzz } from "./fizzbuzz";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#numero");
+const form = document.querySelector("#fizzbuzz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + generarCadenaFizzBuzz(firstNumber) + "</p>";
 });
